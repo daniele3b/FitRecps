46fed18819a6d07448d8e09fdc59d0f244e67924
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-05-26 23:20:34 +0200

    sef

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
      before_action :authenticate_user!
-     before_action :configure_permitted_parameters, :only => [:create]
 
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nome, :cognome, :luogoNascita, :dataNascita, :sesso, :email, :password) }
-    end
 end 

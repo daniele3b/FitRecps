d5da88f74fead47f0928890d3f816be6bd5b6d50
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-26 22:23:27 +0200

    fix

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,8 +10,9 @@ class CardsController < ApplicationController
         @categoria=params[:categoria]
         @esercizi=params[:esercizi]
         @tutorial=params[:tutorial]
+        @energia=params[:energia]
        
-        @card= Card.create(:idf=>@durata,:categoria=>@categoria,:durata=>@durata,:zona=>@zona,:esercizi=>@esercizi,:tutorial=>@tutorial,:user_id=>current_user.id)
+        @card= Card.create(:idf=>@durata,:categoria=>@categoria,:durata=>@durata,:energia=>@energia,:zona=>@zona,:esercizi=>@esercizi,:tutorial=>@tutorial,:user_id=>current_user.id)
         @card.save!
         redirect_to cards_url
     end

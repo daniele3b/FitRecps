f3be059f59841dbbe6500468f5b0f05851d73b24
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-26 13:46:50 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,6 @@ Rails.application.routes.draw do
   resources :cards
   resources :recipes
 
-  devise_scope :user do
-    root :to => 'devise/sessions#new'
-  end
-
-  def after_sign_in_path_for(resource_or_scope)
-    
-  end
-  
-  def after_sign_out_path_for(resource_or_scope)
-   
-  end
+ root 'cards#index'
    
 end

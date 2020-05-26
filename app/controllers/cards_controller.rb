9f68bc421d9d508141b6940641de94d9543f17d8
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-26 18:53:58 +0200

    work post

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,20 @@
 class CardsController < ApplicationController
 
     def index
-       
+       puts(Card.all)
     end
 
     def create
-        redirect_to recipes_url
+        @durata=params[:durata]
+        @zona=params[:zona]
+        @categoria=params[:categoria]
+        @esercizi=params[:esercizi]
+        @tutorial=params[:tutorial]
+       
+        puts(current_user.id)
+       @card= Card.create(:idf=>@durata,:durata=>@durata,:zona=>@zona,:esercizi=>@esercizi,:tutorial=>@tutorial,:user_id=>current_user.id)
+       @card.save!
+        redirect_to cards_url
     end
 
     def new

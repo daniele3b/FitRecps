f1d448cecdc848724ac588c4f233f6c5459cae4d
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-05-27 08:57:09 +0200

    t

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_195740) do
+ActiveRecord::Schema.define(version: 2020_05_27_064649) do
 
   create_table "cards", id: false, force: :cascade do |t|
     t.string "idf"

41c9238d89757bf2aeb74b8d63931d41c7c2f9db
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-05-28 11:28:37 +0200

    added modify

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
         if(Review.where(cards: params[:id],users: current_user.id).length==0)
             redirect_to new_review_path(cid: params[:id])
         else
-            redirect_to edit_review_path
+            redirect_to edit_review_path(cid:params[:id])
         end
 
     end
@@ -32,7 +32,26 @@ class ReviewsController < ApplicationController
 
 
     def edit
-        render html:'dio'
+        @c=params[:cid]
+
+        @rev=Review.where(cards:@c,users:current_user.id)
+
+        @r=@rev[0]
+  
       
+    end
+
+    def update
+        @s=params[:s]
+        puts(@r)
+    
+        @rev=Review.where(cards:@s,users:current_user.id)
+
+        @rev[0].destroy
+        
+        @review=Review.create(:valore=>params[:valore],:cards=>@s,:users=>current_user.id)
+        @review.save!
+
+        redirect_to cards_url
     end
 end

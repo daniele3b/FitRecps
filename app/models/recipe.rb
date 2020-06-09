dd8ff132a223a2d0708b0fbcdfeb981284752c97
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-06-09 15:06:41 +0200

    foto è ora required

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   self.primary_key = 'codice'
   mount_uploader :foto, FotoUploader
-  validates :nome,:ingredienti,:tipo,:proteine,:grassi,:carboidrati,:energia,  presence: true
+  validates :nome,:ingredienti,:tipo,:proteine,:grassi,:carboidrati,:energia,:foto,  presence: true
   belongs_to :user
 end
--- spec/addRating.spec.rb
+++ spec/addRating.spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Search by name a recipe', type: :feature do
   User.delete_all
   m1 = User.new( :id=>'1',:nome=>'bho',:cognome=>'bho9',:luogoNascita=>'ciao',:dataNascita=>'1998/12/2',:sesso=>'m',:email => 'd@gmail.com', :password => '12345678')
   m1.save!
-  r1=Recipe.new( :codice=>'666', :nome=>'Ricetta per nome', :tipo=>'Primo',
+  r1=Recipe.new(:foto=>'https://it.wikipedia.org/wiki/File:Chord_1000_random.png', :codice=>'666', :nome=>'Ricetta per nome', :tipo=>'Primo',
     :ingredienti=>'200gr di guanciale 4 uova pecorino q.b.', :procedimento=>'cucinare con molta attenzione e NO PARMIGIANO', :energia=>55, :carboidrati=>2,
     :grassi=>12, :proteine=>34, :fibre=>234, :sodio=>234,
     :user_id=>m1.id, :interazioni=>0)

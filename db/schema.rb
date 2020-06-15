2912f509632582845a3d3310ba89326383e5b905
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-06-15 21:56:57 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_162250) do
+ActiveRecord::Schema.define(version: 2020_06_15_195535) do
 
   create_table "cards", force: :cascade do |t|
     t.string "idf"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2020_06_14_162250) do
     t.boolean "employee", default: false
     t.string "provider"
     t.string "cf"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

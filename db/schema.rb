15d113be5de86ec1539d735327d23cfb755971c0
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-06-15 14:18:41 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_162250) do
+ActiveRecord::Schema.define(version: 2020_06_15_064832) do
 
   create_table "cards", force: :cascade do |t|
     t.string "idf"
@@ -26,18 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_14_162250) do
     t.string "spiegazione"
     t.integer "interazioni"
     t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
-  create_table "employees", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_employees_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -103,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_06_14_162250) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provide"
     t.string "uid"
     t.string "nome"
     t.string "cognome"
@@ -110,13 +99,11 @@ ActiveRecord::Schema.define(version: 2020_06_14_162250) do
     t.string "dataNascita"
     t.string "sesso"
     t.integer "roles_mask"
-    t.boolean "admin"
-    t.boolean "employee"
-    t.string "avatar"
+    t.boolean "admin", default: false
+    t.boolean "employee", default: false
     t.string "provider"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "cf"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
